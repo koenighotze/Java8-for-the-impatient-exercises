5daf8496f4b0c4915b48c0f4a33c165871185350
David Schmitz <david.schmitz.privat@gmail.com>
2016-04-04 08:34:26 +0200

    Trying to understand crazy Java type system

---
+++ src/test/java/org/koenighotze/chapter3/Ex3_22Test.java
@@ -0,0 +1,71 @@
+package org.koenighotze.chapter3;
+
+import static java.lang.Thread.currentThread;
+import static java.lang.Thread.sleep;
+import static java.util.Arrays.asList;
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static java.util.stream.Collectors.toList;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+import org.junit.Test;
+
+/**
+ * @author David Schmitz
+ */
+public class Ex3_22Test {
+
+    public static void sleeper(TimeUnit unit, long duration) {
+        try {
+            sleep(unit.toMillis(duration));
+        } catch (InterruptedException e) {
+            currentThread().interrupt();
+        }
+    }
+
+    @Test
+    public void trying_flatmap() {
+        // @formatter:off
+        List<String> result = Stream.of("Foo", "Baz")
+                .flatMap(s -> Stream.of(s, s.toLowerCase(), s.toUpperCase()))
+                .collect(toList());
+        // @formatter:on
+        assertThat(result, is(equalTo(asList("Foo", "foo", "FOO", "Baz", "baz", "BAZ"))));
+    }
+
+    @Test
+    public void trying_map() {
+        // @formatter:off
+        List<List<String>> result = Stream.of("Foo", "Baz")
+                .map(s -> Stream.of(s, s.toLowerCase(), s.toUpperCase()))
+                .map(stream -> stream.collect(toList()))
+                .collect(toList());
+        // @formatter:on
+        assertThat(result, is(equalTo(asList(asList("Foo", "foo", "FOO"), asList("Baz", "baz", "BAZ")))));
+    }
+
+    @Test
+    public void map_with_completable_future() throws ExecutionException, InterruptedException {
+        CompletableFuture<Stream<String>> future = supplyAsync(() -> {
+            sleeper(SECONDS, 1);
+            // @formatter:off
+            return
+                Stream.of("Foo", "Quuz");
+            // @formatter:on
+        });
+
+        CompletableFuture<Stream<Integer>> composed =
+            future.thenComposeAsync(stream -> supplyAsync(() -> stream.map(String::length)));
+        List<Integer> result = composed.get().collect(toList());
+        assertThat(result, is(equalTo(asList(3, 4))));
+
+    }
+}
