b0ea9f257c1d6c5c070af2c2278e4ef8fe346bce
David Schmitz <david.schmitz.privat@gmail.com>
2016-04-04 09:53:24 +0200

    Worked on 3.15

---
+++ src/test/java/org/koenighotze/chapter3/Ex3_15AsyncTest.java
@@ -0,0 +1,73 @@
+package org.koenighotze.chapter3;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.toList;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.UnaryOperator;
+
+import org.junit.Test;
+
+/**
+ * @author David Schmitz
+ */
+public class Ex3_15AsyncTest {
+    public static class AsyncStringHandler {
+        private final String source;
+        private final List<UnaryOperator<String>> ops;
+
+        private AsyncStringHandler(String source) {
+            this.source = source;
+            this.ops = new LinkedList<>();
+        }
+
+        public static AsyncStringHandler of(String source) {
+            return new AsyncStringHandler(requireNonNull(source));
+        }
+
+        public AsyncStringHandler then(UnaryOperator<String> operation) {
+            ops.add(operation);
+            return this;
+        }
+
+        public List<String> apply() {
+            if (ops.isEmpty()) {
+                return singletonList(source);
+            }
+
+            return ops.parallelStream().map(op -> op.apply(source)).collect(toList());
+        }
+    }
+
+    @Test
+    public void no_op() {
+        assertThat(AsyncStringHandler.of("a simple sentence").apply(), is(equalTo(singletonList("a simple sentence"))));
+    }
+
+    @Test
+    public void empty_source() {
+        assertThat(AsyncStringHandler.of("").apply(), is(equalTo(singletonList(""))));
+    }
+
+    @Test
+    public void single_transform() {
+        assertThat(AsyncStringHandler.of("Qux").then(String::toLowerCase).apply(), is(equalTo(singletonList("qux"))));
+    }
+
+    @Test
+    public void multi_transform() {
+        // @formatter:off
+        assertThat(AsyncStringHandler.of("Qux")
+            .then(String::toLowerCase)
+            .then(s -> s.substring(1, 3))
+            .apply(), is(equalTo(asList("qux", "ux"))));
+        // @formatter:on
+    }
+
+}
---
+++ src/test/java/org/koenighotze/chapter3/Ex3_15SyncTest.java
@@ -0,0 +1,66 @@
+package org.koenighotze.chapter3;
+
+import static java.util.Objects.requireNonNull;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
+import org.junit.Test;
+
+/**
+ * @author David Schmitz
+ */
+public class Ex3_15SyncTest {
+    public static class StringHandler {
+        private final String source;
+        private Function<String, String> ops = UnaryOperator.identity();
+
+        private StringHandler(String source) {
+            this.source = source;
+        }
+
+        public static StringHandler of(String source) {
+            return new StringHandler(requireNonNull(source));
+        }
+
+        public StringHandler then(UnaryOperator<String> operation) {
+            ops = ops.andThen(operation::apply);
+            return this;
+        }
+
+        public String apply() {
+            return ops.apply(source);
+        }
+    }
+
+    @Test
+    public void no_op() {
+        assertThat(StringHandler.of("a simple sentence").apply(), is(equalTo("a simple sentence")));
+    }
+
+    @Test
+    public void empty_source() {
+        assertThat(StringHandler.of("").apply(), is(equalTo("")));
+    }
+
+    @Test
+    public void single_transform() {
+        assertThat(StringHandler.of("Qux").then(String::toLowerCase).apply(), is(equalTo("qux")));
+    }
+
+    @Test
+    public void multi_transform() {
+        // @formatter:off
+        assertThat(StringHandler.of("Qux")
+            .then(String::toLowerCase)
+            .then(s -> s.substring(1, 3))
+            .apply(), is(equalTo("ux")));
+        // @formatter:on
+    }
+
+}
